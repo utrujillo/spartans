6917f944483dee9fd0d0f5756c87287cf5c15842
BacteriaLoki <ramo.diego12@hotmail.com>
2016-10-17 13:09:11 -0500

    User003 (#12)
    * aaa
    
    * [#131230463]
    Agrege lo de usuarios y modifique las vistas a español
    	new file:   app/views/users/confirmations/new.html.erb
    	new file:   app/views/users/mailer/confirmation_instructions.html.erb
    	new file:   app/views/users/mailer/password_change.html.erb
    	new file:   app/views/users/mailer/reset_password_instructions.html.erb
    	new file:   app/views/users/mailer/unlock_instructions.html.erb
    	new file:   app/views/users/passwords/edit.html.erb
    	new file:   app/views/users/passwords/new.html.erb
    	new file:   app/views/users/registrations/edit.html.erb
    	new file:   app/views/users/registrations/new.html.erb
    	new file:   app/views/users/sessions/new.html.erb
    	new file:   app/views/users/shared/_links.html.erb
    	new file:   app/views/users/unlocks/new.html.erb
    	modified:   config/initializers/devise.rb
    	modified:   db/migrate/20161004191919_devise_create_users.rb
    	modified:   db/schema.rb
    
    * [#131230463]
    cambio en schema
    	modified:   db/schema.rb
    
    * [#131230463]
    	modified:   config/routes.rb
    	modified:   db/schema.rb
    
    * [#131230463] Agrege los cambios de user
    
    	modified:   app/controllers/admin/categories_controller.rb
    	modified:   app/controllers/admin/home_controller.rb
    	modified:   app/controllers/admin/labels_controller.rb
    	modified:   app/controllers/application_controller.rb
    	new file:   app/controllers/users_controller.rb
    	modified:   app/models/user.rb
    	modified:   app/views/layouts/admin/_navbar.html.erb
    	modified:   app/views/users/confirmations/new.html.erb
    	modified:   app/views/users/mailer/confirmation_instructions.html.erb
    	modified:   app/views/users/mailer/password_change.html.erb
    	modified:   app/views/users/mailer/reset_password_instructions.html.erb
    	modified:   app/views/users/mailer/unlock_instructions.html.erb
    	modified:   app/views/users/passwords/edit.html.erb
    	modified:   app/views/users/passwords/new.html.erb
    	modified:   app/views/users/registrations/edit.html.erb
    	modified:   app/views/users/registrations/new.html.erb
    	modified:   app/views/users/sessions/new.html.erb
    	modified:   app/views/users/shared/_links.html.erb
    	modified:   app/views/users/unlocks/new.html.erb
    	modified:   db/schema.rb
    
    * Implementando metodo en modelo para concatenar el nombre de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-
   namespace :admin do
     resources :categories
     resources :labels
     resources :home, only: [:index]
   end
-
+  devise_for :users
   get 'themes/tipo'
   get 'themes/buttons'
   get 'themes/form'
@@ -13,7 +12,3 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 end
-
-
-
-
