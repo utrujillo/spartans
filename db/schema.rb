73fce633216bc6e8c3355696e7d7a6c5ee790e3b
Bryan Roman <aloeroman@gmail.com>
2016-10-25 01:43:10 -0500

    botones terminados y correcion de codigos y conflictos

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160920173636) do
-=======
 ActiveRecord::Schema.define(version: 20161004191919) do
->>>>>>> master
 
   create_table "categories", force: :cascade do |t|
     t.string   "nombre_categoria"
@@ -29,8 +25,6 @@ ActiveRecord::Schema.define(version: 20161004191919) do
     t.datetime "updated_at",      null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -52,5 +46,5 @@ ActiveRecord::Schema.define(version: 20161004191919) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> master
+
 end
