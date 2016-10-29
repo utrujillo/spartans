2f0b8ab21f5c7e2c65e142bdc47a0d144159681d
Uziel Trujillo Colon <uziel.trujillo@hotmail.com>
2016-10-29 01:45:58 -0500

    Modulo de Post (#18)
    * Primer commit modulo post
    
    * corrigiendo errores
    
    *  [#131394093]
      En este commit se agrega al modulo post, las herramientas de la gema
      wysiwyg, al contenido del post se le permite darle los siguientes
      formatos:
         -Negrita
         -Italica
         -Tamaño de texto
         -Colores
         -Comentarios
         -Listas Ordenadas
         -Enlaces
         -Insertar Imagenes
         -Ver codigo
    
    * [#131394093] Modulo Post
    
    Notas:
    - Se agregan los botones de video y fullscreen a froala
    - Se corrigen errores de botones y visualización en form
    
    	modified:   app/assets/javascripts/default.js
    	modified:   app/assets/javascripts/froala.js
    	modified:   app/assets/stylesheets/default.css
    	modified:   app/views/admin/posts/_form.html.erb
    	modified:   app/views/admin/posts/edit.html.erb
    	modified:   app/views/admin/posts/new.html.erb
    	modified:   app/views/admin/posts/show.html.erb

---
+++ app/assets/javascripts/froala.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.froala-editor').froalaEditor({
+    toolbarButtons: ['fullscreen','|','bold','italic','fontSize','color','quote','orderedList','formatOL','insertLink','insertImage','insertVideo','html'],
+    imageInsertButtons: ['imageByURL'],
+    videoInsertButtons: ['videoByURL'],
+    height: 400
+  })
+})
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories
     resources :labels
+    resources :posts
     resources :home, only: [:index]
   end
   devise_for :users
