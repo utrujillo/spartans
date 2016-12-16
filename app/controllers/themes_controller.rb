b5b0bd96d7dab95c8e04abd43c31b9662e9f1a3c
Uziel Trujillo <uziel.trujillo@gmail.com>
2016-12-15 21:00:51 -0600

    Implementando enlace a pagina de inicio

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,10 +1,20 @@
 class ThemesController < ApplicationController
+  before_filter :set_categories
+
   def tipo
   end
-  
+
   def form
   end
-  
+
   def buttons
   end
+
+  def hyperlink
+  end
+
+  private
+    def set_categories
+      @categories = Category.all
+    end
 end
